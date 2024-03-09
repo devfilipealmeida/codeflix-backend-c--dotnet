ef21de1f6fe471f874212b447b1bf4fbe92ca5c8
devfilipealmeida <filipesilva504@gmail.com>
2024-03-08 21:22:44 -0300

    implementada a classe Category e classe de testes de category

---
+++ tests/Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using DomainEntity = Codeflix.Catalog.Domain.Entity;
+
+namespace Codeflix.Catalog.UnitTests.Domain.Entity.Category
+{
+    public class CategoryTest
+    {
+        [Fact(DisplayName = nameof(Instantiate))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void Instantiate()
+        {
+            // Arrange
+            var validData = new
+            {
+                Name = "category name",
+                Description = "caregory description"
+            };
+
+            // Act
+            var category = new DomainEntity.Category(validData.Name, validData.Description);
+            // Assert
+            Assert.NotNull(category);
+            Assert.Equal(validData.Name, category.Name);
+            Assert.Equal(validData.Description, category.Description);
+
+        }
+    }
+}
